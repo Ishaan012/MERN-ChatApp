96f0b0324c7033db7f12df3cdb6a95f6f741861f
Ishaan Rawat <ishaan.rawat.ishu@gmail.com>
2024-08-10 22:19:47 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -10,23 +11,26 @@ import { app, server } from "./socket/socket.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
-const PORT= process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
 app.use(express.json()); //To parse incomming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.get('/',(req,res)=>{
-    //Root route
-    res.send('Hello, World!!');
-})
+app.use(express.static(path.join(__dirname, '/frontend/dist'))); //Used to serve static files like html, css ,img files...
 
-server.listen(PORT,()=>{
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server is running on port ${PORT}`);
 })
